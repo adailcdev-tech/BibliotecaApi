a38fc0c59b8c89e5a1240a3a963f534b8762baf6
adailcdev-tech <adailcdev@gmail.com>
2026-05-29 19:51:20 -0300

    adiciona docker-compose com MongoDB

--- obj/Debug/net10.0/BibliotecaApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/BibliotecaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86abcf7d335d613081865e2dfbdc774a948c49a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ecd9ebb6194b8ae5eadfbf72c8366167d45d93")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
